073c688cdc1828d172f2d6c740578dc05915932b
iamhritik <hritik.99@outlook.com>
2022-10-17 03:21:30 +0000

    imported resources successfully

--- scenarios/import_resources/main.tf
+++ scenarios/import_resources/main.tf
@@ -1,3 +1,30 @@
 resource "aws_instance" "testing" {
-
+    ami                                  = "ami-062df10d14676e201"
+    associate_public_ip_address          = true
+    availability_zone                    = "ap-south-1a"
+    instance_type                        = "t2.micro"
+    key_name                             = "ttn"
+    subnet_id                            = "subnet-0acec19483e2989ef"
+    tags                                 = {
+        "Name" = "testing-server"
+    }
+    tenancy                              = "default"
+    vpc_security_group_ids               = [
+        "sg-0a8554057bfdb46a9",
+    ]
+    metadata_options {
+        http_endpoint               = "enabled"
+        http_put_response_hop_limit = 1
+        http_tokens                 = "optional"
+        instance_metadata_tags      = "disabled"
+    }
+    root_block_device {
+        delete_on_termination = true
+        encrypted             = false
+        iops                  = 100
+        tags                  = {}
+        throughput            = 0
+        volume_size           = 8
+        volume_type           = "gp2"
+    }
 }
